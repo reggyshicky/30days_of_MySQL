3b565288f46f23839beccd758d12af29f205734c
reggyshicky <reginahshikanda@gmail.com>
2023-07-27 21:47:37 +0300

    fetches all columns while limiting the rows to 20

--- fetch_fields.sql
+++ fetch_fields.sql
@@ -1,4 +1,4 @@
 --- Fetches field from a table limiting it to 20 rows
-SELECT *
-FROM city, country, citylanguage
-LIMIT 20;
+SELECT * FROM city LIMIT 20;
+SELECT * FROM country LIMIT 20;
+SELECT * FROM countrylanguage LIMIT 20;
