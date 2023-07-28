824e067ffa25d708de02ede2be01ac3c1bf39014
reggyshicky <reginahshikanda@gmail.com>
2023-07-28 13:43:42 +0300

    insert in a database

---
+++ DAY_2/insert_into.sql
@@ -0,0 +1,6 @@
+--- Inserts 5new rows of data in the city table 
+INSERT INTO city (ID, Name, CountryCode, Population)
+VALUES
+('18887', 'Siaya', 'KEN', 'Nyanza', '693847'),
+('18888', 'Matuu', 'KEN', 'Eastern', '576849'),
+('18889', 'Kiambu', 'KEN', 'Central', '94567');
