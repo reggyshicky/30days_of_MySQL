5679a1e576972e1c2fe701d56f9421f808dd584d
reggyshicky <reginahshikanda@gmail.com>
2023-08-08 13:20:02 +0300

    implementation of the order statement

---
+++ DAY_7/least_populated_cities.sql
@@ -0,0 +1,3 @@
+-- query that returns the ten most populated cities.Order the data
+-- by population from the most populated to the least populated city.
+SELECT Name, Population FROM city ORDER BY Population DESC LIMIT 10;
